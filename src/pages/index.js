5b2102666884ef2f583e32beb130b8ad33e251b2
Bhuvanesh Jagtap <deshmukhkanha25@gmail.com>
2025-12-03 23:03:29 +0530

    Building pages

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Signup from "./Signup";
+import Login from "./Login";
+import Home from "./Home";
+import AllPosts from "./AllPosts";
+import AddPost from "./AddPost";
+import EditPost from "./EditPost";
+
+export default{
+    Signup,
+    Login,
+    Home,
+    AllPosts,
+    AddPost,
+    EditPost
+}
